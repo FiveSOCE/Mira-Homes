6ea71852c6148dc4326bd5973c8d0b904e367452
FiveS <sgmmsionide@gmail.com>
2026-09-03 07:25:15 +1000

    Add MiraHomes plugin bootstrap

---
+++ src/main/java/com/mira/homes/MiraHomesPlugin.java
@@ -0,0 +1,44 @@
+package com.mira.homes;
+
+import com.mira.homes.gui.HomeGuiListener;
+import com.mira.homes.gui.HomeGuiService;
+import com.mira.homes.listener.HomeCommandBridgeListener;
+import com.mira.homes.particle.HomeParticleService;
+import net.ess3.api.IEssentials;
+import org.bukkit.command.PluginCommand;
+import org.bukkit.plugin.Plugin;
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class MiraHomesPlugin extends JavaPlugin {
+    @Override
+    public void onEnable() {
+        saveDefaultConfig();
+
+        Plugin essentialsPlugin = getServer().getPluginManager().getPlugin("Essentials");
+        if (!(essentialsPlugin instanceof IEssentials essentials)) {
+            getLogger().severe("EssentialsX 2.22.0+ is required. Disabling MiraHomes.");
+            getServer().getPluginManager().disablePlugin(this);
+            return;
+        }
+
+        HomeGuiService gui = new HomeGuiService(this, essentials);
+        HomeParticleService particles = new HomeParticleService(this);
+
+        getServer().getPluginManager().registerEvents(new HomeGuiListener(gui, particles), this);
+        getServer().getPluginManager().registerEvents(new HomeCommandBridgeListener(gui), this);
+
+        PluginCommand command = getCommand("mhomes");
+        if (command != null) {
+            command.setExecutor((sender, cmd, label, args) -> {
+                if (!(sender instanceof org.bukkit.entity.Player player)) {
+                    sender.sendMessage("MiraHomes can only be opened by a player.");
+                    return true;
+                }
+                gui.open(player);
+                return true;
+            });
+        }
+
+        getLogger().info("MiraHomes v" + getPluginMeta().getVersion() + " enabled.");
+    }
+}
