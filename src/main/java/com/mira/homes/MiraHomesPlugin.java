1a9e27dda401ebe41596dd1d31b3c31ec4a98a6d
FiveS <sgmmsionide@gmail.com>
2026-09-04 20:04:11 +1000

    Delegate home teleport visuals to MiraCosmetics

--- src/main/java/com/mira/homes/MiraHomesPlugin.java
+++ src/main/java/com/mira/homes/MiraHomesPlugin.java
@@ -3,7 +3,6 @@ package com.mira.homes;
 import com.mira.homes.gui.HomeGuiListener;
 import com.mira.homes.gui.HomeGuiService;
 import com.mira.homes.listener.HomeCommandBridgeListener;
-import com.mira.homes.particle.HomeParticleService;
 import net.ess3.api.IEssentials;
 import net.kyori.adventure.text.serializer.legacy.LegacyComponentSerializer;
 import org.bukkit.command.PluginCommand;
@@ -11,7 +10,8 @@ import org.bukkit.plugin.Plugin;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public final class MiraHomesPlugin extends JavaPlugin {
-    private static final LegacyComponentSerializer LEGACY = LegacyComponentSerializer.legacyAmpersand();
+    private static final LegacyComponentSerializer LEGACY =
+            LegacyComponentSerializer.legacyAmpersand();
     private static final String CHAT_PREFIX = "&5&lMira &8>> &r";
 
     @Override
@@ -26,16 +26,16 @@ public final class MiraHomesPlugin extends JavaPlugin {
         }
 
         HomeGuiService gui = new HomeGuiService(this, essentials);
-        HomeParticleService particles = new HomeParticleService(this);
 
-        getServer().getPluginManager().registerEvents(new HomeGuiListener(gui, particles), this);
+        getServer().getPluginManager().registerEvents(new HomeGuiListener(gui), this);
         getServer().getPluginManager().registerEvents(new HomeCommandBridgeListener(gui), this);
 
         PluginCommand command = getCommand("mhomes");
         if (command != null) {
             command.setExecutor((sender, cmd, label, args) -> {
                 if (!(sender instanceof org.bukkit.entity.Player player)) {
-                    sender.sendMessage(LEGACY.deserialize(CHAT_PREFIX + "&cMiraHomes can only be opened by a player."));
+                    sender.sendMessage(LEGACY.deserialize(
+                            CHAT_PREFIX + "&cMiraHomes can only be opened by a player."));
                     return true;
                 }
                 gui.open(player);
@@ -43,6 +43,7 @@ public final class MiraHomesPlugin extends JavaPlugin {
             });
         }
 
-        getLogger().info("MiraHomes v" + getPluginMeta().getVersion() + " enabled.");
+        getLogger().info("MiraHomes v" + getPluginMeta().getVersion()
+                + " enabled. Successful home teleports are visualized by MiraCosmetics when installed.");
     }
 }
